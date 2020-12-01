a6c06b42b5bcb59b38c11281736559503c4f7cf5
DarthScion <moraru.cosmin1981@yahoo.ro>
2020-12-01 22:08:18 +0200

    Primul meu commit Git :)

---
+++ src/JavaLearning/Invatare.java
@@ -0,0 +1,36 @@
+package JavaLearning;
+
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.AbstractAction;
+
+import java.awt.event.*;
+import java.awt.EventQueue;
+
+import java.util.*;
+import java.util.EventListener;
+import java.util.Arrays;
+import java.util.concurrent.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import java.lang.*;
+import java.lang.Math.*;
+import java.lang.String.*;
+
+
+
+
+public class Invatare 
+{ /* public class Invatare 000 ---> START */
+
+	public static void main(String[] args)
+	{ /* public static void main 000 ---> START */
+		
+		JOptionPane.showMessageDialog(null, "Welcome to Java");
+		
+	} /* public static void main 000 ---> END */
+	
+	
+} /* public class Invatare 000 ---> END */
